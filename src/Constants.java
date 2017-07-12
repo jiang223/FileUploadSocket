0c91b8c8802773d8c64b24804b18da6e83a57863
ll <ll123>
2017-07-12 18:53:45 +0800

    1

---
+++ src/Constants.java
@@ -0,0 +1,14 @@
+
+
+public class Constants {
+
+    public final static String RECEIVE_FILE_PATH = "D:/file_socket/";
+
+    public final static String SEND_FILE_PATH = "E:/gjj3";
+
+    public final static int DEFAULT_BIND_PORT = 10001;
+
+    public static final String SERVER_FILE_PATH = "D:/file_socket/";
+
+
+}
